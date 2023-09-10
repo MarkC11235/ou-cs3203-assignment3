77331de1657f323b5e96111a78d079116ba79c54
Mark Castle <marklouiscastle@gmail.com>
2023-09-10 15:26:02 -0500

    added main method to allow input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-list = [1,2,3,4,5]
+# list = [1,2,3,4,5]
 
 def sumList(list):
     sum = 0
@@ -12,4 +12,14 @@ def multiplyList(list):
         product *= i
     return product
 
+def main():
+    list = []
+    num_elements = input("Enter number of elements in list: ")
+    for i in range(0, int(num_elements)):
+        element = input("Enter element: ")
+        list.append(int(element))
+    
+    print("Sum of list: ", sumList(list))
+    print("Product of list: ", multiplyList(list))
 
+main()
